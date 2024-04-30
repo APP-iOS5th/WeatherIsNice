8e994def297d7b8b49b2a07a742cf5a574039bb0
WonyeolHEO <hyy96hyy@naver.com>
2024-04-30 15:34:54 +0900

    repository initial setting

---
+++ WeatherApi/WeatherApi/WeatherApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApiApp.swift
+//  WeatherApi
+//
+//  Created by wonyoul heo on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
